a7624cc867c445420788a828a17cf97f6054b6a5
fjohnson1991 <felicity.johnson.mail@gmail.com>
2017-01-09 20:39:28 -0500

    fixed banner constraits

--- Studio/BannerCollectionViewCell.swift
+++ Studio/BannerCollectionViewCell.swift
@@ -37,39 +37,54 @@ class BannerCollectionViewCell: UICollectionViewCell {
     
     private func configureView() {
         
+        self.layer.backgroundColor = UIColor.black.withAlphaComponent(0.7).cgColor
+        
         // avatarImage Config
         avatarImageView.translatesAutoresizingMaskIntoConstraints = false
-        avatarImageView = UIImageView(frame: CGRect(x: 20, y: 45, width: 40, height: 40))
+        avatarImageView = UIImageView(frame: CGRect(x: 20, y: 35, width: 40, height: 40))
         avatarImageView.contentMode = .scaleAspectFill
         self.addSubview(avatarImageView)
         
         // usernameLabel Config
         self.addSubview(usernameLabel)
         usernameLabel.translatesAutoresizingMaskIntoConstraints = false
-        usernameLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 20).isActive = true
-        usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 0).isActive = true
-        usernameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 20).isActive = true
+        usernameLabel.heightAnchor.constraint(equalToConstant: 15).isActive = true
         usernameLabel.textAlignment = .center
-        usernameLabel.font = UIFont(name: "Helvetica", size: 14.0)
+        usernameLabel.font = UIFont(name: "Helvetica", size: 15.0)
+        usernameLabel.textColor = UIColor.white
         
         // ageGenderLocation Config
         self.addSubview(ageGenderLocation)
         ageGenderLocation.translatesAutoresizingMaskIntoConstraints = false
-        ageGenderLocation.topAnchor.constraint(equalTo: usernameLabel.bottomAnchor, constant: 20).isActive = true
-        ageGenderLocation.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 0).isActive = true
-        ageGenderLocation.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 20).isActive = true
+        ageGenderLocation.heightAnchor.constraint(equalToConstant: 15).isActive = true
         ageGenderLocation.textAlignment = .center
-        ageGenderLocation.font = UIFont(name: "Helvetica", size: 14.0)
+        ageGenderLocation.font = UIFont(name: "Helvetica", size: 12.0)
+        ageGenderLocation.textColor = UIColor.white
         
         // bpmLabel Config
         self.addSubview(bpmLabel)
         bpmLabel.translatesAutoresizingMaskIntoConstraints = false
-        bpmLabel.topAnchor.constraint(equalTo: ageGenderLocation.bottomAnchor, constant: 20).isActive = true
-        bpmLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 0).isActive = true
-        bpmLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 20).isActive = true
+        bpmLabel.heightAnchor.constraint(equalToConstant: 15).isActive = true
         bpmLabel.textAlignment = .center
-        bpmLabel.font = UIFont(name: "Helvetica", size: 14.0)
-
+        bpmLabel.font = UIFont(name: "Helvetica", size: 12.0)
+        bpmLabel.textColor = UIColor.white
+        
+        // stack view Config
+        let stackView = UIStackView()
+        stackView.axis = UILayoutConstraintAxis.vertical
+        stackView.distribution  = UIStackViewDistribution.equalSpacing
+        stackView.alignment = UIStackViewAlignment.center
+        stackView.spacing = 12.0
+        
+        stackView.addArrangedSubview(usernameLabel)
+        stackView.addArrangedSubview(ageGenderLocation)
+        stackView.addArrangedSubview(bpmLabel)
+        stackView.translatesAutoresizingMaskIntoConstraints = false;
+        self.addSubview(stackView)
+        
+        // stack view constraints
+        stackView.topAnchor.constraint(equalTo: self.topAnchor, constant: 20).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 8).isActive = true
     }
     
     func populateUserInfo() {
